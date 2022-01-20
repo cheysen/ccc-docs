6642c8f8cc1c80be743fbb1e6493167443204fa7
chenhailin <chenhailin@mucfc.com>
2022-01-20 17:19:14 +0800

    修改网站根目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   title: "ccc-docs",
   description: 'cheysen的个人技术知识文档库',
-  base: '/ccc-docs/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  //base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/assets/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,blog,doc,document,cheysen,heysen' }],
